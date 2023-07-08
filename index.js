92131fc0b05e75553a31771252482905f51a9ec5
PedroArthur0 <136533459+PedroArthur0@users.noreply.github.com>
2023-07-07 23:41:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
-var SetaDireita = window.document.getElementById("seta-direita")
-var leonardo = window.document.getElementById("leonardo")
-var samantha = window.document.getElementById("samantha")
-var bruna = window.document.getElementById("bruna")
-var SetaEsquerda = window.document.getElementById("seta-esquerda")
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var imagem1 = window.document.getElementById("imagem")
+var imagem2 = window.document.getElementById("imagem2")
 
 function RolarParaDireita() {
-    leonardo.style = "display:none"
-    bruna.style = "display:flex"
-    SetaEsquerda.style = "display:flex"
-    SetaDireita.style = "display:none; margin-top 20px"
+    imagem1.style = "display:none"
+    imagem2.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
 }
 
 function RolarParaEsquerda() {
-    leonardo.style = "display:flex"
-    bruna.style = "display:none"
-    SetaEsquerda.style = "display:none"
-    SetaDireita.style = "display:flex; margin-top 20px"
+    imagem1.style = "display:flex"
+    imagem2.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var imagem1 = window.document.getElementById("imagem")
+var imagem2 = window.document.getElementById("imagem2")
+
+function RolarParaDireita() {
+    imagem1.style = "display:none"
+    imagem2.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    imagem1.style = "display:flex"
+    imagem2.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var imagem1 = window.document.getElementById("imagem")
+var imagem2 = window.document.getElementById("imagem2")
+
+function RolarParaDireita() {
+    imagem1.style = "display:none"
+    imagem2.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    imagem1.style = "display:flex"
+    imagem2.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var imagem1 = window.document.getElementById("imagem")
+var imagem2 = window.document.getElementById("imagem2")
+
+function RolarParaDireita() {
+    imagem1.style = "display:none"
+    imagem2.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    imagem1.style = "display:flex"
+    imagem2.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
 }
